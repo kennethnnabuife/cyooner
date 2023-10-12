b97e7c943e1c90f17e709828f7e3898f99989d73
Kenneth <nnabuifekenneth766@gmail.com>
2023-10-12 04:20:59 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header-and-hero">
+        <div className="overlay"></div>
+        <div className="header">
+          <img
+            src="https://res.cloudinary.com/dynmdbdfu/image/upload/v1697070770/cyoonerlogo_PNG_xpaz2h.png"
+            alt="Cyooner"
+          />
+
+          <div className="hamburger-icon" onClick={toggleMenu}>
+            <div className={`line ${showMenu ? "active" : ""}`}></div>
+            <div className={`line ${showMenu ? "active" : ""}`}></div>
+            <div className={`line ${showMenu ? "active" : ""}`}></div>
+          </div>
+        </div>
+
+        <div className="hero">
+          <div className="sub-hero">
+            <div className="hero-main-text">
+              We make quality furnitures and interiors that create a feel of
+              elegance to your home and office
+            </div>
+          </div>
+        </div>
+
+        {showMenu && (
+          <div className="menu-items">
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
