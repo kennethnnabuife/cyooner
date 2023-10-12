4299db14312e36c7e4c39e58ddc48f4e4e85ce07
Kenneth <nnabuifekenneth766@gmail.com>
2023-10-12 13:13:54 +0100

    redid

--- src/App.js
+++ src/App.js
@@ -10,37 +10,35 @@ function App() {
 
   return (
     <div>
-      <div className="header-and-hero">
+      <div className="header">
+        <img
+          src="https://res.cloudinary.com/dynmdbdfu/image/upload/v1697070770/cyoonerlogo_PNG_xpaz2h.png"
+          alt="Cyooner"
+        />
+
+        <div className="hamburger-icon" onClick={toggleMenu}>
+          <div className={`line ${showMenu ? "active" : ""}`}></div>
+          <div className={`line ${showMenu ? "active" : ""}`}></div>
+          <div className={`line ${showMenu ? "active" : ""}`}></div>
+        </div>
+      </div>
+
+      {showMenu && (
+        <div className="menu-items">
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+        </div>
+      )}
+
+      <div className="hero-div">
         <div className="overlay"></div>
-        <div className="header">
-          <img
-            src="https://res.cloudinary.com/dynmdbdfu/image/upload/v1697070770/cyoonerlogo_PNG_xpaz2h.png"
-            alt="Cyooner"
-          />
-
-          <div className="hamburger-icon" onClick={toggleMenu}>
-            <div className={`line ${showMenu ? "active" : ""}`}></div>
-            <div className={`line ${showMenu ? "active" : ""}`}></div>
-            <div className={`line ${showMenu ? "active" : ""}`}></div>
+        <div className="sub-hero">
+          <div className="hero-main-text">
+            We make quality furnitures and interiors that create a feel of
+            elegance to your home and office!
           </div>
         </div>
-
-        <div className="hero">
-          <div className="sub-hero">
-            <div className="hero-main-text">
-              We make quality furnitures and interiors that create a feel of
-              elegance to your home and office
-            </div>
-          </div>
-        </div>
-
-        {showMenu && (
-          <div className="menu-items">
-            <a href="#">Home</a>
-            <a href="#">About</a>
-            <a href="#">Contact</a>
-          </div>
-        )}
       </div>
     </div>
   );
